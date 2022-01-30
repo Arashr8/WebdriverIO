b244bbfec8666343a3d33779cc2db7662f5ff6b9
Arashr8 <arash1ra@hotmail.com>
2022-01-30 23:57:19 +0100

    test one more button

--- test/specs/firstPage.js
+++ test/specs/firstPage.js
@@ -7,23 +7,32 @@ describe('Volvo Cars', () => {
             'A million more | Volvo Cars - International');
     });
     it('should click on the cookies button', async () => {
-    const acceptCookie = await $('.accept-cookie-container')   
+    const acceptCookie = await $('.accept-cookie-container');  
     acceptCookie.click();
     });
 
     it('should click on the menu button', async () => {
-        const menuBtn = await $('#sitenav-sidenav-toggle')
+        const menuBtn = await $('#sitenav-sidenav-toggle');
         menuBtn.click();
     });
 
     it('should click on the Buy button', async () => {
-        const buyBtn = await $('._SN-aw=Buy')
+        const buyBtn = await $('._SN-aw=Buy');
         buyBtn.click();
     });
 
+    it('should click on the Car Configurator button', async () => {
+        const confBtn = await $('Car Configurator');
+        confBtn.click();
+    });
+
     it('should click on the Our Cars button', async () => {
-        const car = await $('._SN-aq=Our Cars')
+        const car = await $('._SN-aq=Our Cars');
         car.click();
+    });
+
+    it('should redirect to a new page and verify the title', async () => {
+       expect(browser).toHaveTitle('Design your Volvo | Volvo Cars International');
     });
 });
 
